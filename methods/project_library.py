438f9c17975e344d49ef48e1afa7dc6d74160496
Chaofan Wang <chaofanqw@gmail.com>
2019-03-19 16:09:39 +1100

    Plot offset result.

--- methods/project_library.py
+++ methods/project_library.py
@@ -1,7 +1,9 @@
 import ntplib
 import time
-import datetime
 import csv
+import matplotlib.pyplot as plt
+import os
+import pandas
 
 
 def get_time_offset():
@@ -10,20 +12,38 @@ def get_time_offset():
     return response.tx_time, response.offset
 
 
-if __name__ == '__main__':
+def plot_result():
+    result = pandas.read_csv('../data/data_diff.csv')
+    time_offset = list(result['TimeOffset'])
+    plt.plot([x for x in range(0, len(time_offset))], time_offset)
+    plt.show()
+
+
+def generate_result():
     times = 180
     time_diff = []
 
     for each in range(0, times):
-        time_diff.append([*get_time_offset()])
-        print(time_diff[-1])
-        time.sleep(10)
+        try:
+            time_diff.append([*get_time_offset()])
+            print(each, time_diff[-1])
+            time.sleep(10)
+        except:
+            continue
 
     f = open('../data/data_diff.csv', 'w')
     time_writer = csv.writer(f)
     time_writer.writerow(['Time', 'TimeOffset'])
 
-    for each in range(0, times):
+    for each in range(0, len(time_diff)):
         time_writer.writerow(time_diff[each])
 
     f.close()
+
+
+if __name__ == '__main__':
+    if not os.path.exists('../data/data_diff.csv'):
+        generate_result()
+    else:
+        plot_result()
+
