21d2cd7e02252d2339690acd6d833d4e528b091d
Chaofan Wang <chaofanqw@gmail.com>
2019-03-19 15:13:37 +1100

    Correction of ntp-timestamp

--- methods/project_library.py
+++ methods/project_library.py
@@ -1,7 +1,29 @@
 import ntplib
+import time
+import datetime
+import csv
 
 
 def get_time_offset():
     c = ntplib.NTPClient()
     response = c.request('au.pool.ntp.org', version=3)
-    return response.offset
+    return response.tx_timestamp, response.offset
+
+
+if __name__ == '__main__':
+    times = 180
+    time_diff = []
+
+    for each in range(0, times):
+        time_diff.append([*get_time_offset()])
+        print(time_diff[-1])
+        time.sleep(10)
+
+    f = open('../data/data_diff.csv', 'w')
+    time_writer = csv.writer(f)
+    time_writer.writerow(['Time', 'TimeOffset'])
+
+    for each in range(0, times):
+        time_writer.writerow(time_diff[each])
+
+    f.close()
