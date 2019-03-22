fd94f65a0397a839dad193fbf59f9c48a78019d0
Chaofan Wang <chaofanqw@gmail.com>
2019-03-22 14:15:14 +1100

    bug-fix

--- methods/interface_mac.py
+++ methods/interface_mac.py
@@ -31,13 +31,15 @@ def connection_mac(pipe):
         response = json.loads(response)
 
         if response['status'] == 'start':
-            scheduler = sched.scheduler(time.time, time.sleep)
+            # scheduler = sched.scheduler(time.time, time.sleep)
             _, time_offset = project_library.get_time_offset()
-            func = lambda pipe, response: pipe.send(response['message'])
+            # func = lambda pipe, response: pipe.send(response['message'])
 
             now = datetime.datetime.timestamp(datetime.datetime.now())
             sleep = response['time'] - (now + time_offset)
-            scheduler.enter(sleep, 1, func, argument=(pipe, response))
+            # scheduler.enter(sleep, 1, func, argument=(pipe, response))
+            time.sleep(sleep)
+            pipe.send(response['message'])
 
         elif response['status'] == 'end':
             pipe.send({'status': 'end'})
