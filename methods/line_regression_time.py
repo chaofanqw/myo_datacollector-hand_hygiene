1de3aa212e0483f5625169e5970c433fc9c591ce
Chaofan Wang <chaofanqw@gmail.com>
2019-04-23 14:03:31 +1000

    New function: Linear regression for time shifting

--- methods/line_regression_time.py
+++ methods/line_regression_time.py
@@ -3,18 +3,25 @@ import pandas as pd
 import methods.project_library as time_shift
 import numpy
 from matplotlib import pyplot as plt
+import datetime
 
 estimators = TheilSenRegressor()
 
 result = pd.read_csv('../data/data_diff.csv')
 timestamp = numpy.array(result['Time']).reshape(-1, 1)
 time_offset = result['TimeOffset']
+time_result = [datetime.datetime.fromtimestamp(each).strftime('%H:%M') for each in list(result['Time'])]
 
 estimators.fit(timestamp, time_offset)
-plt.plot(result['Time'], estimators.predict(timestamp))
-plt.plot(result['Time'], time_offset)
-plt.show()
+
+plt.xticks(range(0, len(time_result), 1000), [time_result[each] for each in range(0, len(time_result), 1000)])
+plt.plot(time_offset)
+plt.plot(estimators.predict(timestamp))
 
 time_predicted = time_shift.get_time_offset()
 result = estimators.predict(numpy.array(time_predicted[0]).reshape(-1, 1))
-print(result, time_predicted[1])
+print('Prediction(Predicted, NTPlib):', result, time_predicted[1])
+print('Daily time shifting', estimators.coef_[0] * 60 * 60 * 24)
+
+plt.show()
+
